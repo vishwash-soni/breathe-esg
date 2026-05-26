915d39350bea9037aec162cff36ababd58ae286f
anubhav-soni <130144941+vishwash-soni@users.noreply.github.com>
2026-05-26 22:26:47 +0530

    added rest framework

--- backend/settings.py
+++ backend/settings.py
@@ -31,15 +31,15 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
-    'rest_framework',
-    'emissions',
-    'corsheaders',
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    "corsheaders",
+    "rest_framework",
+    "emissions",
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
 ]
 
 MIDDLEWARE = [
